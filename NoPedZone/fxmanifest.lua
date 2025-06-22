d903d794da36afe96518fe34b8665462308ff058
Specs <lewisheath.ur@gmail.com>
2025-06-22 11:17:21 +0100

    Delete NoPedZone directory

--- NoPedZone/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-
-author 'Specs'
-description 'NoPedZone - Prevents ped and vehicle spawns in custom polyzones. Configurable, lightweight, and easy to use.'
-version '1.0.0'
-
-client_script 'client.lua'
-shared_script 'config.lua'
